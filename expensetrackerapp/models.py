8b7a9a296fc765fdf08768b463296b845bd9ce96
Maymouna <maalmethen@gmail.com>
2023-12-29 11:58:32 +0300

    create models

--- expensetrackerapp/models.py
+++ expensetrackerapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    published_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=PROTECT)
+    distribution_expense = models.FloatField()
+    
+    def __str__(self):
+        return self.title
